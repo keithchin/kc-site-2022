1e9b1c2f10e01dba5585a1b9c62a27bd50a07648
keithchin <kcisbug@gmail.com>
2022-05-06 16:47:39 +0800

    Initial Commit

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import type { NextPage } from 'next'
+
+const Header: FC= () => {
+    return (
+        <header>
+            
+        </header>
+    )
+}
+
+export default Header;
