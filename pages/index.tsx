ff52c08a96fecaea35a0f391d0ee548c7bc05b47
keithchin <kcisbug@gmail.com>
2022-10-19 23:59:49 +0800

    Added Bio section

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,14 +27,19 @@ const Home: NextPage = () => {
       </section>
       <section id="bio" className="bio max-height-100">
         <div className="bio__wrapper">
-          <h2>Bio</h2>
-          <p>Welcome to my website.</p>
-          <p>I'm Keith Chin, or KC for short.</p>
+          <h2>Hello there, good to see you around here..</h2>
+          <p className="text-xl">I'm Keith Chin, but you can call me KC.</p>
+          <p className="text-xl">Who am I? I'm a problem solving creature that is always scratching that logical or creative itch at the back of my mind.</p>
+          <p className="text-xl">That aside, my current profession is a Front End Developer, where I bring to life design wireframes with a soul flowing in from the Back End.   </p>
+          <p className="text-xl">If you're looking for my <span className="font-semibold">resume</span> or <span className="font-semibold">portfolio</span>, feel free to jump to the <a href="#work" className="font-bold hover:text-slate-300">Work</a> section.</p>
+          <p className="text-xl">If you're here, however, for my music, do check out the <a href="#music" className="font-bold hover:text-slate-300">Music</a> section</p>
+          <p className="text-xl">Feel free to wonder around and you can also contact me here. Just make sure you're not a scammer. </p>
+          <p className="font-bold text-3xl">Enjoy your stay!</p>
         </div>
       </section>
       <div id="work" className="max-height-100  bg-black"></div>
       <div id="contact" className="max-height-100  bg-white"></div>
-      <div id="disco" className="max-height-100  bg-black"></div>
+      <div id="music" className="max-height-100  bg-black"></div>
       <Footer />
     </div>
   )
