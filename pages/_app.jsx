db69ac84b034c7ff544a747f8d4c229ccce19183
Keith Chin <keith@dropee.com>
2022-10-20 23:14:35 +0800

    Added framer-motion

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,27 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import { AnimatePresence } from 'framer-motion'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-      <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet"></link>
       </Head>
       <Header />
-      <Component {...pageProps} />
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Component {...pageProps} />
+      </AnimatePresence>
+      <Footer />
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect } from 'react';
 import type { NextPage } from 'next'
-import Footer from '../components/Footer'
+import Layout from '../components/Layout'
 
 
 // const handleScroll = () => {}
@@ -14,20 +14,21 @@ import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <section id="hero" className="max-height-100 hero-image" >
-        <div className='tw'>
-          <div className='tw__wrapper'>
-            <Image src="/logo_keith.png" alt="Keith Chin Logo" width={500} height={100} />
-            <a className='tw__programmer' href="#work">Front End Developer. Music Maker. Audiophile.</a>
+    <Layout title="Home" description="Keith Chin | Front End Developer. Music Maker. Audiophile">
+      <div>
+        <section>
+          <div className='tw'>
+            <div className='tw__wrapper'>
+              <Image src="/logo_keith.png" alt="Keith Chin Logo" width={500} height={100} />
+              <a className='tw__programmer' href="#work">Front End Developer. Music Maker. Audiophile.</a>
+            </div>
           </div>
-        </div>
-      </section>
-      <div id="work" className="max-height-100  bg-black"></div>
-      <div id="contact" className="max-height-100  bg-white"></div>
-      <div id="music" className="max-height-100  bg-black"></div>
-      <Footer />
-    </div>
+        </section>
+        <div id="work" className="max-height-100  bg-black"></div>
+        <div id="contact" className="max-height-100  bg-white"></div>
+        <div id="music" className="max-height-100  bg-black"></div>
+      </div>
+    </Layout>
   )
 }
 
