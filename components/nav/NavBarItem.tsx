d18abdce81c52cda0a3bd927654d509dcb3d2904
Keith Chin <keith@dropee.com>
2022-10-21 15:43:48 +0800

    Playing around w framer motion on navbar

--- components/nav/NavBarItem.tsx
+++ components/nav/NavBarItem.tsx
@@ -1,6 +1,7 @@
-import React , { FC }from 'react';
+import React, { FC } from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import { motion } from 'framer-motion'
 
 interface INavBarItem {
     name: string;
@@ -17,14 +18,20 @@ const NavBarItem: FC<INavBarItem> = (props) => {
     //         <li>{props.name}</li>
     //     );
     // }
-        return (
+    return (
+        <motion.div
+            className="box"
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 5 }}
+        >
             <li className='nav__item'>
                 <Link href={props.url}>
-                {props.name}
+                    {props.name}
 
                 </Link>
             </li>
-        );
+        </motion.div>
+    );
 }
 
 export default NavBarItem;
--- pages/bio.tsx
+++ pages/bio.tsx
@@ -1,17 +1,26 @@
 import type { NextPage } from 'next'
+import { motion } from 'framer-motion'
 
 const Bio: NextPage = () => {
     return (
+
         <section id="bio" className="bio max-height-100">
             <div className="bio__wrapper">
-            <h2>Hello there, good to see you around here..</h2>
-            <p className="text-xl">I'm Keith Chin, but you can call me KC.</p>
-            <p className="text-xl">Who am I? I'm a problem solving creature that is always scratching that logical or creative itch at the back of my mind.</p>
-            <p className="text-xl">That aside, my current profession is a Front End Developer, where I bring to life design wireframes with a soul flowing in from the Back End.   </p>
-            <p className="text-xl">If you're looking for my <span className="font-semibold">resume</span> or <span className="font-semibold">portfolio</span>, feel free to jump to the <a href="#work" className="font-bold hover:text-slate-300">Work</a> section.</p>
-            <p className="text-xl">If you're here, however, for my music, do check out the <a href="#music" className="font-bold hover:text-slate-300">Music</a> section</p>
-            <p className="text-xl">Feel free to wonder around and you can also contact me here. Just make sure you're not a scammer. </p>
-            <p className="font-bold text-3xl">Enjoy your stay!</p>
+                <motion.div
+                    initial={{ scale: 0 }}
+                    animate={{ rotate: 0, scale: 1 }}
+                    transition={{ delay: 0.5, type: 'spring' }}
+                >
+                    <h2 className="font-bold text-3xl">Hello there, good to see you around here..</h2>
+                    <p className="text-xl mt-5">I'm Keith Chin, but you can call me KC.</p>
+                    <p className="text-xl mt-5">Who am I? I'm a problem solving creature that is always scratching that logical or creative itch at the back of my mind.</p>
+                    <p className="text-xl mt-5">That aside, my current profession is a Front End Developer, where I bring to life design wireframes with a soul flowing in from the Back End.   </p>
+                    <p className="text-xl mt-5">If you're looking for my <span className="font-semibold">resume</span> or <span className="font-semibold">portfolio</span>, feel free to jump to the <a href="#work" className="font-bold hover:text-slate-300">Work</a> section.</p>
+                    <p className="text-xl mt-5">If you're here, however, for my music, do check out the <a href="#music" className="font-bold hover:text-slate-300">Music</a> section</p>
+                    <p className="text-xl mt-5">Feel free to wonder around and you can also contact me here. Just make sure you're not a scammer. </p>
+                    <p className="font-bold text-3xl mt-10">Enjoy your stay!</p>
+                </motion.div>
+
             </div>
         </section>
     )
